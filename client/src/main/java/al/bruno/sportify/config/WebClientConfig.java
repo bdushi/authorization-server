406d29968b5791b1b365f69f1e87fb3bef5c8855
Bruno <bruno@Brunos-MacBook-Pro.local>
2024-02-28 17:47:36 +0100

    Implementing Google as Identity Provider using Spring

--- client/src/main/java/al/bruno/sportify/config/WebClientConfig.java
+++ client/src/main/java/al/bruno/sportify/config/WebClientConfig.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package al.bruno.tracker.config;
+package al.bruno.sportify.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- identity/src/main/java/al/bruno/identity/security/DefaultSecurityConfig.java
+++ identity/src/main/java/al/bruno/identity/security/DefaultSecurityConfig.java
@@ -1,16 +1,16 @@
-package al.bruno.identity.config;
+package al.bruno.identity.security;
 
-import al.bruno.identity.security.FederatedIdentityConfigure;
+import al.bruno.identity.federated.FederatedIdentityAuthenticationSuccessHandler;
 import al.bruno.identity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @EnableWebSecurity
@@ -19,6 +19,25 @@ public class DefaultSecurityConfig {
 
     @Autowired
     public UserService userService;
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/assets/**", "/webjars/**", "/login").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .loginPage("/login")
+                                .successHandler(authenticationSuccessHandler())
+                ).build();
+    }
 //    @Bean
 //    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 //        http
@@ -27,33 +46,14 @@ public class DefaultSecurityConfig {
 //                                .requestMatchers("/assets/**", "/webjars/**", "/login").permitAll()
 //                                .anyRequest().authenticated()
 //                )
-//                .formLogin(formLogin ->
-//                        formLogin
-//                                .loginPage("/login")
-//                )
-//                .oauth2Login(oauth2Login ->
-//                        oauth2Login
-//                                .loginPage("/login")
-//                                .successHandler(authenticationSuccessHandler())
-//                );
-//
+//                .formLogin(Customizer.withDefaults())
+//                .cors(Customizer.withDefaults())
+//                .apply(new FederatedIdentityConfigure().oauth2UserHandler(userService));
 //        return http.build();
 //    }
-    @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/assets/**", "/webjars/**", "/login").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .cors(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults())
-                .apply(new FederatedIdentityConfigure()
-                        .oauth2UserHandler(userService));
-        return http.build();
+    private AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new FederatedIdentityAuthenticationSuccessHandler();
     }
-
     @Bean
     public SessionRegistry sessionRegistry() {
         return new SessionRegistryImpl();
